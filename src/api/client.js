2cb331b068b20ceb38bc6b65c5b47c2d95409f74
ruchita0131 <ruchitasaraf9@gmail.com>
2026-03-13 19:30:48 +0000

    fix api base url

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://smartemi-api.onrender.com",
   headers: { 'Content-Type': 'application/json' }
 });
 
